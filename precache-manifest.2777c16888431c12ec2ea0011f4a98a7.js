059e0cf950f362bbb4edf23ac039b6fc3d232538
Danusshkumar <112367768+Danusshkumar@users.noreply.github.com>
2024-09-18 20:20:56 +0530

    Updates

---
+++ precache-manifest.2777c16888431c12ec2ea0011f4a98a7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f88a93e0f21afb069880",
+    "url": "/blogger/static/css/main.8a76ba37.chunk.css"
+  },
+  {
+    "revision": "f88a93e0f21afb069880",
+    "url": "/blogger/static/js/main.e743fa44.chunk.js"
+  },
+  {
+    "revision": "1ba68dc7c4d4cffeadb2",
+    "url": "/blogger/static/js/runtime~main.669a5cdf.js"
+  },
+  {
+    "revision": "8bf29604c9036e6b647e",
+    "url": "/blogger/static/css/2.7ebdd758.chunk.css"
+  },
+  {
+    "revision": "8bf29604c9036e6b647e",
+    "url": "/blogger/static/js/2.69ab7a0b.chunk.js"
+  },
+  {
+    "revision": "2d89b49ac28614e9ccd9c81308b5120c",
+    "url": "/blogger/static/media/fa-regular-400.2d89b49a.woff2"
+  },
+  {
+    "revision": "715d593456fa02fe72a008a72398f5be",
+    "url": "/blogger/static/media/fa-brands-400.715d5934.woff2"
+  },
+  {
+    "revision": "370dd5af19f8364907b6e2c41f45dbbf",
+    "url": "/blogger/static/media/fa-regular-400.370dd5af.ttf"
+  },
+  {
+    "revision": "38fec6307db2547752379b51322ded10",
+    "url": "/blogger/static/media/fa-v4compatibility.38fec630.ttf"
+  },
+  {
+    "revision": "237f4a0afbdb652fb2330ee7e1567dd3",
+    "url": "/blogger/static/media/fa-solid-900.237f4a0a.woff2"
+  },
+  {
+    "revision": "56c8d80832e37783f12c05db7c8849e2",
+    "url": "/blogger/static/media/fa-brands-400.56c8d808.ttf"
+  },
+  {
+    "revision": "7c428138d15b872f193c40828f6fa0d3",
+    "url": "/blogger/static/media/fa-v4compatibility.7c428138.woff2"
+  },
+  {
+    "revision": "adec7d6f310bc577f05e8fe06a5daccf",
+    "url": "/blogger/static/media/fa-solid-900.adec7d6f.ttf"
+  },
+  {
+    "revision": "95608cbac3826eb4d7e34ccc48591030",
+    "url": "/blogger/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/blogger/precache-manifest.2777c16888431c12ec2ea0011f4a98a7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/blogger/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
